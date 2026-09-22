23669643e5f9b79a9094ad26d2320b2416201fd5
NyxInTime <nyx@nicintime.ca>
2026-09-21 22:29:47 -0400

    basic size of file done

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
-use clap::{Parser, Subcommand, ValueEnum};
+use clap::{Parser, ValueEnum};
+use colored_text::Colorize;
+use humansize::{BINARY, format_size};
+use std::{fs, path::PathBuf};
 
-#[derive(Parser)]
+/// Simple program to get the size of a file
+#[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short)]
+    /// Whether to print in tree format or not
+    #[arg(short, default_value_t = false)]
     tree: bool,
+
+    file: Option<PathBuf>,
 }
 
 #[derive(ValueEnum, Clone, Debug)]
 enum Commands {}
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let args = Args::parse();
     match args.tree {
-        true => todo!(),
-        false => todo!(),
+        true => Ok(()),
+        false => {
+            let file = match args.file {
+                Some(ref f) => f,
+                None => &".".into(),
+            };
+            let metadata = match fs::metadata(&file) {
+                Ok(m) => m,
+                Err(e) => return Err(e),
+            };
+            let len = format_size(metadata.len(), BINARY);
+            let file_colored = match metadata.is_dir() {
+                true => file.display().blue(),
+                false => file.display().white(),
+            };
+
+            println!("{} | {}", file_colored, len);
+            Ok(())
+        }
     }
 }
